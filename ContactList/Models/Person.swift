eba9be984d0f0463b8c0be480b103087512e47c2
denkitdam <cryptoden145@gmail.com>
2023-04-18 15:54:42 +0300

    Model setup

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -5,4 +5,27 @@
 //  Created by Denis Kitaev on 18.04.2023.
 //
 
-import Foundation
+struct Person {
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
+    
+    static func getPersons() -> [Person] {
+        var persons: [Person] = []
+        for _ in 1...names.count {
+            let person = Person(
+                name: names.randomElement() ?? "",
+                surname: surnames.randomElement() ?? "",
+                phone: phones.randomElement() ?? "",
+                email: emails.randomElement() ?? ""
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+
