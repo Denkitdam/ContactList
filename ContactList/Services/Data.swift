714887a7cc59666a76dc6ecb6a70f530834e8f05
denkitdam <cryptoden145@gmail.com>
2023-04-20 17:40:31 +0300

    directory name changed created a class dataStore

---
+++ ContactList/Services/Data.swift
@@ -0,0 +1,39 @@
+//
+//  Data].swift
+//  ContactList
+//
+//  Created by Denis Kitaev on 18.04.2023.
+//
+class DataStore {
+    let names = [
+        "Mike",
+        "Ann",
+        "John",
+        "Steven",
+        "Jack",
+        "Sarah"
+    ]
+    let surnames = [
+        "Miller",
+        "Jackson",
+        "Hudson",
+        "Craig",
+        "Cox",
+        "Mendez"
+    ]
+    let phones = [
+        "00445789",
+        "00234321",
+        "00650932",
+        "00541232",
+        "00431324",
+        "00456578"
+    ]
+    let emails = [
+        "aa@gmail.com",
+        "gtt@gmail.com",
+        "rtf@yahoo.com",
+        "thg@outlook.com",
+        "str@gmail.com"
+    ]
+}
