4a48023791e8426e4147aba1d2c154954d074785
denkitdam <cryptoden145@gmail.com>
2023-04-20 22:49:41 +0300

    made a section for contacts

--- ContactList/ViewControllers/SectionsListViewController.swift
+++ ContactList/ViewControllers/SectionsListViewController.swift
@@ -21,15 +21,23 @@ class SectionsListViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        persons.
+        2
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Section contacts", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let person = persons[indexPath.section]
+        var content = cell.defaultContentConfiguration()
         
-
+        switch indexPath.row {
+        case 0:
+            content.text = "Phone: \(person.phone)"
+        default:
+            content.text = "Email: \(person.email)"
+        }
+        
+        cell.contentConfiguration = content
         return cell
     }
     
